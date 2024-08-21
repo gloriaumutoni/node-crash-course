5ca13e294aec63b9c5964261831c30de36849269
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-08-21 17:28:23 +0200

    requests and responses

--- server.js
+++ server.js
@@ -2,10 +2,29 @@ let http = require("http");
 let fs = require("fs");
 
 const server = http.createServer((req, resp) => {
+  console.log(req.url, req.method);
   resp.setHeader("Content-Type", "text/html");
   // resp.write('<h1>hello,again</h1>')
-
-  fs.readFile("./views/index.html", (err, data) => {
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      resp.statusCode=200
+      break;
+    case "/about":
+      path += "about.html";
+      resp.statusCode=200
+      break;
+      case "/about-me":
+        resp.setHeader('Location','/about')
+      resp.statusCode=301
+      break;
+    default:
+      path += "404.html";
+      resp.statusCode=404
+      break
+  }
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       resp.end();
