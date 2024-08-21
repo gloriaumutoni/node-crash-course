0d80c62e4f40749576b254d1b8e982fe63e23a3b
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-08-21 19:53:39 +0200

    npm

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
-let http = require("http");
-let fs = require("fs");
+const http = require("http");
+const fs = require("fs");
+const _ = require("lodash");
 
 const server = http.createServer((req, resp) => {
-  console.log(req.url, req.method);
+  //lodash
+  const num = _.random(0, 20);
+  console.log(num);
+
+  //you can't call the function more than once
+  const greet = _.once(() => {
+    console.log("hello");
+  });
+  greet();
+
+  //   console.log(req.url, req.method);
+
   resp.setHeader("Content-Type", "text/html");
   // resp.write('<h1>hello,again</h1>')
+
   let path = "./views/";
   switch (req.url) {
     case "/":
       path += "index.html";
-      resp.statusCode=200
+      resp.statusCode = 200;
       break;
     case "/about":
       path += "about.html";
-      resp.statusCode=200
+      resp.statusCode = 200;
       break;
-      case "/about-me":
-        resp.setHeader('Location','/about')
-      resp.statusCode=301
+    case "/about-me":
+      resp.setHeader("Location", "/about");
+      resp.statusCode = 301;
       break;
     default:
       path += "404.html";
-      resp.statusCode=404
-      break
+      resp.statusCode = 404;
+      break;
   }
   fs.readFile(path, (err, data) => {
     if (err) {
@@ -32,7 +45,6 @@ const server = http.createServer((req, resp) => {
       resp.end(data);
     }
   });
-  //   resp.end();
 });
 
 server.listen(3000, "localhost", () => {
