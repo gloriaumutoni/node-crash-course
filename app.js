6d406d3e6ce5e251473ce4adb9494fd57d10271f
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-08-22 20:46:20 +0200

    using ejs

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-const express=require('express')
-const app=express()
-app.listen(3000)
-app.get('/',(req,resp)=>{
-    resp.sendFile('./views/index.html',{root:__dirname})
-})
-app.get('/about',(req,resp)=>{
-    resp.sendFile('./views/about.html',{root:__dirname})
-})
-app.get('/about-us',(req,resp)=>{
-    resp.redirect('/about')
-})
-app.use((req,resp)=>{
-    resp.status(404).sendFile('./views/404.html',{root:__dirname})
-})
+const express = require("express");
+//express app
+const app = express();
+//listen for requests
+app.listen(3000);
+let blogs = [
+  { title: "luigi", blog: "lorem ipsulm luigi" },
+  { title: "mario", blog: "lorem ipsulm mario" },
+  { title: " third racer", blogs: "lorem ipsulm third racer" },
+];
+app.set("view engine", "ejs");
+app.get("/", (req, resp) => {
+  resp.render("index", { title: "Home", blogs });
+});
+app.get("/about", (req, resp) => {
+  resp.render("about");
+});
+app.get("/about-us", (req, resp) => {
+  resp.redirect("about");
+});
+app.use((req, resp) => {
+  resp.status(404).render("404");
+});
