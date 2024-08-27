5c150b7dc86ec940b79b866baef59e77bd2b28fa
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-08-27 09:01:33 +0200

    using MongoDB to store blogs

--- app.js
+++ app.js
@@ -1,16 +1,54 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
+const { result } = require("lodash");
 //express app
 const app = express();
 //listen for requests
-app.listen(3000);
+
+// connecting database
+const dbURI =
+  "mongodb+srv://gloriaumutoni54:fFF2RWZYkKFP1p6o@cluster0.8k6l5.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0";
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => err);
 let blogs = [
   { title: "luigi", blog: "lorem ipsulm luigi" },
   { title: "mario", blog: "lorem ipsulm mario" },
   { title: " third racer", blogs: "lorem ipsulm third racer" },
 ];
 app.set("view engine", "ejs");
+
+app.use(morgan("dev"));
+app.use(express.static("public"));
+
+app.get("/add-blog", (req, resp) => {
+  const blog = new Blog({
+    title: "new Blog 3",
+    snippet: "about my new blog",
+    body: "more about my new blog",
+  });
+  blog
+    .save()
+    .then((result) => resp.send(result))
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.get("/", (req, resp) => {
-  resp.render("index", { title: "Home", blogs });
+  resp.redirect("/blogs");
+});
+app.get("/blogs", (req, resp) => {
+  Blog.find().sort({createdAt:-1})
+    .then((result) => {
+      resp.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 app.get("/about", (req, resp) => {
   resp.render("about");
@@ -18,6 +56,21 @@ app.get("/about", (req, resp) => {
 app.get("/about-us", (req, resp) => {
   resp.redirect("about");
 });
+app.get("/all-blogs", (req, resp) => {
+  Blog.find()
+    .then((result) => resp.send(result))
+    .catch((err) => {
+      console.log(err);
+    });
+});
+app.get("/single-blog", (req, resp) => {
+  Blog.findById("66cd7320951703e0da4ae5c2")
+    .then((result) => resp.send(result))
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.use((req, resp) => {
   resp.status(404).render("404");
 });
