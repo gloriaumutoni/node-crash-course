c3f4a762f773ecf5bccf9851ba5035add9bb1f79
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-09-02 08:36:45 +0200

    using MVC for redability and reusability

--- routes/blogroutes.js
+++ routes/blogroutes.js
@@ -1,59 +1,12 @@
-const express = require("express");
-const Blog = require("../models/blog");
+const express = require('express');
+const blogController = require('../controllers/blogController');
+
 const router = express.Router();
-const blogController = require("../controllers/blogController");
 
-router.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create a new blog" });
-});
-router.get("/blogs", blogController.blog_index);
+router.get('/create', blogController.blog_create_get);
+router.get('/', blogController.blog_index);
+router.post('/', blogController.blog_create_post);
+router.get('/:id', blogController.blog_details);
+router.delete('/:id', blogController.blog_delete);
 
-router.post("/blogs", (req, resp) => {
-  const Blog = new Blog(req.body);
-  Blog.save()
-    .then((result) => {
-      resp.redirect("/blogs");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-router.get("/blogs/:id", (req, resp) => {
-  const { id } = req.params;
-  Blog.findById(id)
-    .then((result) => {
-      resp.render("partials/details", {
-        blog: result,
-        title: "Blog Details",
-        id,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-router.delete("/blogs/:id", (req, resp) => {
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-    .then((result) => resp.json({ redirect: "/blogs" }))
-    .then((err) => {
-      console.log(err);
-    });
-});
-
-router.get("/about", (req, resp) => {
-  resp.render("about");
-});
-router.get("/about-us", (req, resp) => {
-  resp.redirect("about");
-});
-router.get("/all-blogs", (req, resp) => {
-  Blog.find()
-    .then((result) => resp.send(result))
-    .catch((err) => {
-      console.log(err);
-    });
-});
 module.exports = router;
